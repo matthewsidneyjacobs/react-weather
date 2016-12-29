aa86b50decc9d9585f381f09bdc2a574db1c3f74
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-12-29 00:25:01 -0700

    modified about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,7 +12,10 @@ var React = require('react');
 //you can refactor this code becuase this is a stateless functional Component.  it doesn't care about state
 var About =  (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h3>About</h3>
+      <p>Welcome to the About Page</p>
+  </div>
   )
 };
 
