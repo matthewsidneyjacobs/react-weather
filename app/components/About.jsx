86ba7a6ce3c29228c6db81a22e0712e902920c39
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-12-30 15:46:55 -0700

    more changes with about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,20 +13,35 @@ var React = require('react');
 var About =  (props) => {
   return (
     <div>
-      <h3>About</h3>
+      <h1 className="text-center">About</h1>
       <p>Welcome to the About Page</p>
       <p>This is a picture of an okapi, nothing to do with weather- I'm just playing around with foundation features.</p>
       <div className="card">
-        <div className="card-header">
+        <div className="card-divider">
           Here is an okapi. Super rare, found in small portion of Africa.
         </div>
         <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Okapi2.jpg/220px-Okapi2.jpg' alt="okapi"/>
+        {/* why does taking an image from the web work but not local files? */}
         {/* <img source={require('image!/images/okapi.jpg')} alt="okapi"/> */}
         <div className="card-section">
           <h5>OKAPI</h5>
           <p>They have a prehensile tongue, which allows them to strip leaves off of branches with ease.</p>
         </div>
       </div>
+
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the
+            JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used
+            Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
   </div>
   )
 };
