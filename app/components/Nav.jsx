df474e66fd6e0990a261b6ded85bcaf0d5df3719
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-12-30 10:35:33 -0700

    style nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -18,17 +18,54 @@ var {Link, IndexLink} = require('react-router');
 // });
 
 
-var Nav = (props) => {
-  return (
-    <div>
-        <h2> Nav component </h2>
-        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-        <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}> About </Link>
-        <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}> Examples </Link>
-
-    </div>
-
-  )
-}
+var Nav = React.createClass ({
+  onSearch: function(e) {
+    e.preventDefault();
+    alert('not yet wired up');
+  },
+  render: function () {
+    return (
+      //jsx quirk- must use className instead of class. otherwise there is a conflict with jsx?
+      <div className= "top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}> About </Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}> Examples </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search Weather"/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    )
+  }
+})
 
 module.exports = Nav;
+
+//
+// var old = (
+// <div>
+//     <h2> Nav component </h2>
+//
+//
+//
+// </div>
+// );
