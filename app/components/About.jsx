8ad1e2499c81ce614323b9ca2c55614751700826
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-12-30 15:32:43 -0700

    trying out images

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-
+// var okapi = require('/images/okapi.jpg').Image;
 // var About = React.createClass({
 //   render: function() {
 //     return (
@@ -15,6 +15,18 @@ var About =  (props) => {
     <div>
       <h3>About</h3>
       <p>Welcome to the About Page</p>
+      <p>This is a picture of an okapi, nothing to do with weather- I'm just playing around with foundation features.</p>
+      <div className="card">
+        <div className="card-header">
+          Here is an okapi. Super rare, found in small portion of Africa.
+        </div>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Okapi2.jpg/220px-Okapi2.jpg' alt="okapi"/>
+        {/* <img source={require('image!/images/okapi.jpg')} alt="okapi"/> */}
+        <div className="card-section">
+          <h5>OKAPI</h5>
+          <p>They have a prehensile tongue, which allows them to strip leaves off of branches with ease.</p>
+        </div>
+      </div>
   </div>
   )
 };
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -21,6 +21,7 @@ var Main =  (props) => {
       <div className="row">
         <div className="columns medium-6 large-4 small-centered">
           {props.children}
+          
         </div>
       </div>
     </div>
