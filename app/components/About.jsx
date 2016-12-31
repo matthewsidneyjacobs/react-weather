b2534a4e7426a6fa83003e6f90fe47fa3e19bf8d
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-12-30 18:57:26 -0700

    changed styles further

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,7 +13,7 @@ var React = require('react');
 var About =  (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>Welcome to the About Page</p>
       <p>This is a picture of an okapi, nothing to do with weather- I'm just playing around with foundation features.</p>
       <div className="card">
